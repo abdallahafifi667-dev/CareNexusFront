3a5c5fc536a11f5beddec34494fe8d97b6d1be01
abdallahafifi667-dev <abdallahafifi667@gmail.com>
2026-04-22 02:24:31 +0200

    feat: implement static site generation with react-snap and update SEO meta tags and structured data

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { hydrateRoot, createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
 import App from './App.jsx'
 import './shared/i18n/i18n'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+const appContent = (
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(rootElement, appContent);
+} else {
+  createRoot(rootElement).render(appContent);
+}
